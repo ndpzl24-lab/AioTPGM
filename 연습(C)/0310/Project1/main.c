12b8b933bcd5229dbff64d698db5e1135830a40a
ndpzl24-lab <ndpzl24@gmail.com>
2026-03-17 11:40:02 +0900

    Add files via upload

---
+++ 연습(C)/0310/Project1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "a.h"
+
+int main()
+{
+	printf("대한민국\n");
+	a();
+	printf("화이팅\n");
+}
---
+++ 연습(C)/0310/Untitled4.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*void swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int main()
+{
+	int x = 10;
+	int y = 20;
+	printf("swap 전 : x = %d y = %d\n", x, y);
+	swap(&x,&y);
+	printf("swap 후 : x = %d y = %d\n", x, y);
+	return 0;
+}*/
+
+void swap(int a, int b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+int main()
+{
+	int x = 10;
+	int y = 20;
+	printf("swap 전 : x = %d y = %d\n", x, y);
+	swap(x,y);
+	printf("swap 후 : x = %d y = %d\n", x, y);
+	return 0;
+}
